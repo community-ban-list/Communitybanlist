4914ee894165c87f8c68274c469d8dab016a85a3
werewolfboy13 <werewolfboy13@afocommunity.com>
2023-05-13 19:23:42 -0500

    Adding updated discord invite.

---
+++ web-server/ecosystem.config.js
@@ -0,0 +1,17 @@
+/**
+ * @file PM2 Configuration File.
+ */
+module.exports = {
+	apps: [
+		{
+			name: "CBL Web Server",
+			script: "./ban-importer/index.js",
+			listen_timeout: 3000,
+			wait_ready: true,
+			log_date_format: "YYYY-MM-DD HH:mm Z",
+			env: {
+				NODE_ENV: "production",
+			},
+		},
+	],
+};
