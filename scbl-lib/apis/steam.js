c813ceba0ececd90f60c71b8e048c3094f71bfa9
Michael-Phoenix <43548840+Michael-Phoenix@users.noreply.github.com>
2023-07-18 01:44:43 +0200

    use axios timeout

--- scbl-lib/apis/steam.js
+++ scbl-lib/apis/steam.js
@@ -5,6 +5,7 @@ import { STEAM_API_KEY } from '../config.js';
 
 if (!STEAM_API_KEY) throw new Error('Environmental variable STEAM_API_KEY must be provided.');
 const STEAM_API_RESERVIOR = 200;
+const STEAM_API_RETRIES = 3;
 
 const makeRequest = new Bottleneck({
   reservoir: STEAM_API_RESERVIOR,
@@ -13,7 +14,8 @@ const makeRequest = new Bottleneck({
   minTime: 1500 //300s per 200 requests as a rule of thumb to stop hitting burst rate limit on Steam
 }).wrap(async (method, url, params, data = {}) => {
   return await axios({
-    method,
+    method: method,
+    timeout: 5000,
     url: 'http://api.steampowered.com/' + url,
     params: { ...params, key: STEAM_API_KEY },
     data
