a6d3c414db841e9e85867c815ea43abb0e436f7a
Michael-Phoenix <43548840+Michael-Phoenix@users.noreply.github.com>
2023-07-20 23:53:58 +0200

    improve error msgs

--- ban-importer/src/core.js
+++ ban-importer/src/core.js
@@ -429,7 +429,7 @@ export default class Core {
       Logger.verbose(
         'Core',
         1,
-        `Failed to send Discord Webhook: ${exportBan.ExportBanList.discordWebhook} for List "${exportBan.ExportBanList.name}". See logs above to see which webhook to take action on.`,
+        `Failed to send Discord Webhook: ${exportBan.ExportBanList.discordWebhook} for List "${exportBan.ExportBanList.name}" (ID: ${exportBan.id}).`,
         err
       );
     }
@@ -462,7 +462,7 @@ export default class Core {
       Logger.verbose(
         'Core',
         1,
-        `Failed to send Discord Webhook: ${exportBan.ExportBanList.discordWebhook} for List "${exportBan.ExportBanList.name}". See logs above to see which webhook to take action on.`,
+        `Failed to send Discord Webhook: ${exportBan.ExportBanList.discordWebhook} for List "${exportBan.ExportBanList.name}" (ID: ${exportBan.id}).`,
         err
       );
     }
