a2c92112f88f87ed947b332f7cc05ba612d9e404
Michael-Phoenix <43548840+Michael-Phoenix@users.noreply.github.com>
2023-07-20 19:32:24 +0200

    removed webhook from code; pushing it to env

--- scbl-lib/utils/logger.js
+++ scbl-lib/utils/logger.js
@@ -6,19 +6,24 @@ class Logger {
     this.verboseness = {};
     this.colors = {};
     this.discordHook = createDiscordWebhookMessage(
-      `https://discord.com/api/webhooks/1131519627608993843/UtaS_uOs7lCLQNE7r9--QVcinHqwKpy5g11gKdvDz_k02uUKMl0Axx4TAotChe-VjfUw`,
+      process.env.DISCORD_LOG_WEBHOOK ||
+        `https://discord.com/api/webhooks/1131519627608993843/UtaS_uOs7lCLQNE7r9--QVcinHqwKpy5g11gKdvDz_k02uUKMl0Axx4TAotChe-VjfUw`,
       { retryOnLimit: true }
     )[0];
   }
 
   async verbose(module, verboseness, message, ...extras) {
+    try {
+      if (verboseness === 1) await this.discordHook.send(`[${module}][${verboseness}] ${message}`);
+    } catch (e) {
+      console.log('Error sending Discord Log Message.', e);
+    }
+
     let colorFunc = chalk[this.colors[module] || 'white'];
     if (typeof colorFunc !== 'function') colorFunc = chalk.white;
 
     if ((this.verboseness[module] || 2) >= verboseness)
       console.log(`[${colorFunc(module)}][${verboseness}] ${message}`, ...extras);
-
-    if (verboseness === 1) await this.discordHook.send(`[${module}][${verboseness}] ${message}`);
   }
 
   setVerboseness(module, verboseness) {
