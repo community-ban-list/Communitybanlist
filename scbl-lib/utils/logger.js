b8c8cf4f71ca9bdc3522db652ea2232baec8e806
Michael-Phoenix <43548840+Michael-Phoenix@users.noreply.github.com>
2023-07-21 18:54:02 +0200

    fix width

--- scbl-lib/utils/logger.js
+++ scbl-lib/utils/logger.js
@@ -6,7 +6,9 @@ function convertToBarChart(minValue = 0, maxValue = Infinity, barValue) {
   if (minValue === maxValue) minValue = 0;
   const scaleFactor = (chartWidth - 1) / (maxValue - minValue);
   const barSize = Math.max(Math.ceil((barValue - minValue) * scaleFactor), 1);
-  const chart = `\`\`\`[${'#'.repeat(barSize)}] ${(barValue / maxValue).toFixed(2)}%\`\`\``;
+  const chart = `\`\`[${'#'.repeat(barSize)}${' '.repeat(chartWidth - barSize)}] ${(
+    barValue / maxValue
+  ).toFixed(2)}%\`\``;
 
   return chart;
 }
