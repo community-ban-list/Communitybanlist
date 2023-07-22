ff3562f1019e5f3150807386e95f2180b3f64b91
Michael-Phoenix <43548840+Michael-Phoenix@users.noreply.github.com>
2023-07-22 09:56:18 +0200

    empty bars = 100%

--- scbl-lib/utils/logger.js
+++ scbl-lib/utils/logger.js
@@ -8,7 +8,7 @@ function convertToBarChart(minValue = 0, maxValue = Infinity, barValue) {
   const scaleFactor = chartWidth / ((maxValue || 1) - minValue);
   const barSize = Math.max(Math.ceil((barValue - minValue) * scaleFactor), 1);
   const chart = `\`\`[${'█'.repeat(barSize)}${' '.repeat(chartWidth - barSize)}] ${(
-    (barValue / (maxValue || 1)) *
+    ((barValue || 1) / (maxValue || 1)) *
     100
   ).toFixed(2)}%\`\``;
 
