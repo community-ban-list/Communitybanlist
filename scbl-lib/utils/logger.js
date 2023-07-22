6313148479083853fb6adf55af03c52b23efdacf
Michael-Phoenix <43548840+Michael-Phoenix@users.noreply.github.com>
2023-07-22 09:58:42 +0200

    fix empty bars

--- scbl-lib/utils/logger.js
+++ scbl-lib/utils/logger.js
@@ -6,7 +6,7 @@ function convertToBarChart(minValue = 0, maxValue = Infinity, barValue) {
   const chartWidth = 50;
   if (minValue === maxValue) minValue = 0;
   const scaleFactor = chartWidth / ((maxValue || 1) - minValue);
-  const barSize = Math.max(Math.ceil((barValue - minValue) * scaleFactor), 1);
+  const barSize = Math.max(Math.ceil(((barValue || 1) - minValue) * scaleFactor), 1);
   const chart = `\`\`[${'█'.repeat(barSize)}${' '.repeat(chartWidth - barSize)}] ${(
     ((barValue || 1) / (maxValue || 1)) *
     100
