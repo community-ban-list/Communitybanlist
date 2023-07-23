648d12f64b9b91ef5bad7f6bb1fa03f6f32826c9
Michael-Phoenix <43548840+Michael-Phoenix@users.noreply.github.com>
2023-07-23 16:30:54 +0200

    fix rl + retries

--- scbl-lib/apis/steam.js
+++ scbl-lib/apis/steam.js
@@ -8,12 +8,26 @@ if (!STEAM_API_KEY) throw new Error('Environmental variable STEAM_API_KEY must b
 const STEAM_API_RESERVIOR = 200;
 const STEAM_API_RETRIES = 3;
 
-const makeRequest = new Bottleneck({
+const rl = new Bottleneck({
   reservoir: STEAM_API_RESERVIOR,
   reservoirRefreshAmount: STEAM_API_RESERVIOR,
   reservoirRefreshInterval: 300000,
   minTime: 1500 //300s per 200 requests as a rule of thumb to stop hitting burst rate limit on Steam
-}).wrap(async (method, url, params, data = {}) => {
+});
+
+rl.on('failed', async (error, jobInfo) => {
+  const id = jobInfo.options.id;
+  console.warn(`Job ${id} failed`, error);
+
+  if (jobInfo.retryCount <= 5) {
+    console.log(`Retrying job ${id} in 1s!`);
+    return 1000;
+  } else throw error;
+});
+
+rl.on('retry', (error, jobInfo) => console.log(`Now retrying ${jobInfo.options.id}`));
+
+const makeRequest = rl.wrap(async (method, url, params, data = {}) => {
   console.log('starting steam axios request');
   const retVar = await axios({
     method: method,
@@ -26,16 +40,6 @@ const makeRequest = new Bottleneck({
   console.log('done with steam axios request');
   return retVar;
 });
-makeRequest.on('failed', async (error, jobInfo) => {
-  const id = jobInfo.options.id;
-  console.warn(`Job ${id} failed`, error);
-
-  if (jobInfo.retryCount <= 5) {
-    console.log(`Retrying job ${id} in 1s!`);
-    return 1000;
-  } else throw error;
-});
-makeRequest.on('retry', (error, jobInfo) => console.log(`Now retrying ${jobInfo.options.id}`));
 
 export default async function (method, url, params, data = {}, priority = 5) {
   return await makeRequest.withOptions({ priority }, method, url, params, data);
