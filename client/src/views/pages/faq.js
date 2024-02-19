8084284336dace3ced3faddf9be0761557185fb4
werewolfboy13 <werewolfboy13@afocommunity.com>
2024-02-19 14:04:00 -0600

    spaaaacccciiinnngggg

--- client/src/views/pages/faq.js
+++ client/src/views/pages/faq.js
@@ -83,8 +83,8 @@ const sections = [
             CommunityBanList.com List assigns ban reasons to one or more categorises based on
             keywords/phrases found in the ban reasons assigned by our partner organisations. Our
             keywords and phrases are not perfect so there may be a small number of scenarios where
-            ban reasons cannot be categorised so the reason is shown as "Unknown". This does &nbsp;
-            <strong>NOT</strong> mean a player was banned for no reason.
+            ban reasons cannot be categorised so the reason is shown as "Unknown". This does
+            &nbsp;<strong>NOT</strong> mean a player was banned for no reason.
           </>
         )
       }
