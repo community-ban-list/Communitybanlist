f52b13ddbf085428a3512daf43acb89b75de614b
Marek <werewolfboy13@afocommunity.com>
2024-05-07 00:16:37 -0500

    temporary change to invite link

--- scbl-lib/config.js
+++ scbl-lib/config.js
@@ -19,7 +19,7 @@ const BATTLEMETRICS_ORGANIZATION = process.env.BATTLEMETRICS_ORGANIZATION || '56
 const STEAM_API_KEY = process.env.STEAM_API_KEY;
 
 // Other constants
-const DISCORD_INVITE = process.env.DISCORD_INVITE || 'https://discord.gg/aSvFw6wDcM';
+const DISCORD_INVITE = process.env.DISCORD_INVITE || 'about:blank';
 
 export {
   HOST,
