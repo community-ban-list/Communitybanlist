01c9345444d75e4ced1e46293b3a89e999574e4b
werewolfboy13 <werewolfboy13@afocommunity.com>
2024-09-15 20:55:39 -0500

    Bringing back to prod

--- ban-importer/index.js
+++ ban-importer/index.js
@@ -9,11 +9,11 @@ import ExportBanManager from './src/export-ban-manager.js';
 Error.stackTraceLimit = 255;
 
 const TASKS_TO_COMPLETE = {
-  IMPORT_BANS: false,
+  IMPORT_BANS: true,
   UPDATE_STEAM_USER_INFO: true, // TODO: This is causing random hangs; Try ading more debug code until we can find out what causes it.
   UPDATE_REPUTATION_POINTS: true,
   UPDATE_REPUTATION_RANK: true,
-  UPDATE_EXPORT_BANS: false,
+  UPDATE_EXPORT_BANS: true,
   EXPORT_EXPORT_BANS: true
 };
 async function doSleep(ms) {
